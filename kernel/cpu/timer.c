2755bc4a974fa2c5f266df8301d4c840e0e95d43
Noverisp3 <nguyenpham2044@gmail.com>
2026-05-31 22:57:24 +0700

    fix(timer): implement integer ceiling in sleep_ms to prevent early wakeups

--- kernel/cpu/timer.c
+++ kernel/cpu/timer.c
@@ -9,10 +9,22 @@
 static volatile unsigned int tick_count;
 static unsigned int tick_ms;
 
+static __attribute__((always_inline)) inline unsigned int timer_get_tick()
+{
+    unsigned int tick;
+    __asm__ volatile("mov %%eax, %%ecx;"
+                     "lock xaddl %%ecx, %0;"
+                     "mov %%ecx, %%eax;"
+                     : "=m" (tick_count), "=a" (tick)
+                     : "m" (tick_count)
+                     : "ecx", "memory");
+    return tick;
+}
+
 static void timer_handler(registers_t *regs)
 {
     (void)regs;
-    tick_count++;
+    (void)timer_get_tick();
 }
 
 void init_timer(unsigned int freq)
@@ -35,6 +47,7 @@ unsigned int get_ticks(void)
 
 void sleep_ms(unsigned int ms)
 {
-    unsigned int target = tick_count + (ms + tick_ms - 1) / tick_ms;
-    while (tick_count < target);
+    unsigned int start = tick_count;
+    unsigned int ticks_to_wait = (ms + tick_ms - 1) / tick_ms;
+    while ((tick_count - start) < ticks_to_wait);
 }
